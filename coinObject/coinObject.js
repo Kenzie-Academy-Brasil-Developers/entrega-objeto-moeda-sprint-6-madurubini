b3e88a2ac725f02ea760bceb6edadc2c6b7cca7e
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-18 20:50:26 -0300

    Funções de mostrar na tela feitas, alguns bugs com a visualização em css

--- coinObject/coinObject.js
+++ coinObject/coinObject.js
@@ -21,11 +21,10 @@ const coin = {
   },
 
   toHTML: function () {
+    const painel = document.getElementById('painel')
     const image = document.createElement("img");
-    const moeda = document.getElementById('moeda')
+
     
-
-
     if(coin.toString() === "Heads"){
         image.src = "./assets/cara.png"
         image.alt = "Heads"
@@ -39,12 +38,35 @@ const coin = {
 
 
     }
-    return moeda.appendChild(image)
+    return painel.appendChild(image)
     ;
   },
   
 };
 
 
-
+function display20Flips() {
+    const results = [];
+    for(let i = 0; i < 20; i++){
+        let atual = coin.toString()
+        results.push(atual)  
+    }
+    let bloco = document.createElement('div')
+    let resultado = document.createElement("p")
+    bloco.classList.add('bloco')
+    bloco.appendChild(resultado)
+    resultado.innerHTML = results
+    resultado.classList.add('texto')
+    painel.appendChild(bloco)
+    return results
+  }
+  
+  function display20Images() {
+    const results = [];
+    for(let i = 0; i < 20; i++){
+        let atual = coin.toHTML()
+        results.push(atual)  
+    }
+    return results
+  }
 
